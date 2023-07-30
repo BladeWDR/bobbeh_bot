93b3f466c5604235f1b6a20db07ee65a9b6ce19d
Scott <ssbarning@gmail.com>
2023-07-29 22:12:28 -0400

    various changes to env structure

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 # Variables 
-# TOKEN: Final = '6133928212:AAFDKoye-sJVvmLdPHbXO0Z1xBIKjTwwBFc'
 TOKEN: Final = os.environ.get("TELEGRAM_API_KEY")
-BOT_USERNAME: Final = "@Bobbeh_bot"
+BOT_USERNAME: Final = os.environ.get("TELEGRAM_BOT_USERNAME")
 TIKTOK_RESPONSES = ["https://i.imgur.com/lzWLI6A.jpg", "https://i.imgur.com/3Ytx3vy.jpeg"]
 SQUADOS = ["Poop is one of life's greatest gifts.", "J'onn is getting excited."]
 
